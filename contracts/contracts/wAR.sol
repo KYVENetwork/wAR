e4f8cebbbd9816bf725cac47c00a501f2b083d2d
Fabian Riewe <fabianriewe00@gmail.com>
2021-05-12 20:12:29 +0200

    chore: added comments

--- contracts/contracts/wAR.sol
+++ contracts/contracts/wAR.sol
@@ -7,18 +7,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract wAR is ERC20, Ownable {
 
+  // Burn event which will be picked up the bridge
   event Burn(address sender, string wallet, uint256 amount);
 
   constructor() ERC20("Wrapped AR", "wAR") {}
 
+  // AR has 12 decimals
   function decimals() public view virtual override returns (uint8) {
     return 12;
   }
 
+  // the owner of the contract can mint a specific amount of tokens an address
   function mint(address to, uint256 amount) public onlyOwner {
     _mint(to, amount);
   }
 
+  // the owner can burn a specific amount of wAR from the sender
+  // and emit a Burn event
   function burn(uint256 amount, string memory wallet) public {
     _burn(msg.sender, amount);
     emit Burn(msg.sender, string(wallet), amount);
